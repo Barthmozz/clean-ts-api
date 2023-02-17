2605281329d2e7f4db402eb3fe23e810fac633db
Barthmozz <barthmozz@gmail.com>
2023-02-17 00:01:53 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'rX1Sw-pY1IIp0T65MZgGb_iBEwml34r1dqhkezzoizYgmI5w_aZkAKv8gi4jd_ReRpH55pVuA_8u9j7ZbBY8y2Vtk1aXQbg'
 }
